b62454a0baf348cfa17039a2efbf07db8c396e96
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-06 10:35:20 -0300

    last commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,6 +18,34 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: Column(children: [
+            UserAccountsDrawerHeader(
+                currentAccountPicture: ClipRRect(
+                  child: Image.network(
+                      'https://www.usatoday.com/gcdn/-mm-/5367a8ddcfb9c1f4d81b7884d83903b596f09e0d/c=15-0-465-600/local/-/media/USATODAY/GenericImages/2014/03/16//1394994103000-AP-Music-Tyler-The-Creator-Arrest.jpg'),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                accountName: Text('Tiago'),
+                accountEmail: Text('tiagorodriguessousa9@gmail.com')),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              subtitle: Text('Texto de inicio'),
+              onTap: () {
+                print('Home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Sair'),
+              subtitle: Text('Sair do app'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            )
+          ]),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:statefull_widget/home_page.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -12,44 +11,74 @@ class _LoginState extends State<Login> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  Widget bodyScaffold() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            width: 250,
+            height: 250,
+            child: Image.asset('assets/placeholder.png'),
+          ),
+          Card(
+              child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                  controller: emailController,
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                )
+              ],
+            ),
+          )),
+          ElevatedButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.amber)),
+              onPressed: () {
+                String email = emailController.text;
+                String password = passwordController.text;
+                if (email.isNotEmpty &&
+                    email == 'tiagorodriguessousa9@gmail.com' &&
+                    password.isNotEmpty &&
+                    password == '123') {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else {
+                  print('Bosta');
+                }
+              },
+              child: const Text('Login'))
+        ]),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                  labelText: 'Email', border: OutlineInputBorder()),
-              controller: emailController,
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  labelText: 'Password', border: OutlineInputBorder()),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  String email = emailController.text;
-                  String password = passwordController.text;
-                  if (email.isNotEmpty &&
-                      email == 'tiagorodriguessousa9@gmail.com' &&
-                      password.isNotEmpty &&
-                      password == '123') {
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  } else {
-                    print('Bosta');
-                  }
-                },
-                child: const Text('Login'))
-          ]),
+        body: Stack(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            'assets/landscape.png',
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-    );
+        bodyScaffold(),
+      ],
+    ));
   }
 }
